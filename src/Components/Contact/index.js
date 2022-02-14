6ea136f830147fd3ef5b9dc2cd7dfa069f41af96
Anurag Sahu <anuragsahu060197@gmail.com>
2022-02-14 20:15:34 +0530

    bhut sare changes dobara

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -14,7 +14,7 @@ function Contact() {
                         <span className="contact-span"><b>MISSION-</b> To provide servies of the highest possible standards with quality, safety and reliability by a dedicated team of competent, skilled and committed professionals for all.</span><br/>
                         <span className="contact-span"><b>VALUES-</b> Integrity, compassion and commitment for patient care</span>
                     </div>
-                    <div className="col-md-4 text-center" data-aos="fade-left" data-aos-duration="1000">
+                    <div className="col-md-4 text-center" data-aos="fade-up" data-aos-duration="1000">
                         <h3><CallIcon style={{ width: 40, height: 40, color: 'white' }}/> Contact Us</h3>
                         <a href="tel:+4733378901"><strong>Eye Care Service:</strong> +4733378901</a>
                         <a href="tel:+4733378901"><strong>Dental Service:</strong> +4733378901</a>
@@ -23,7 +23,7 @@ function Contact() {
                             <strong>Address:</strong> Ring road, Manendragarh, Koriya, Chhattisgarh
                         </a>
                     </div>
-                    <div className="col-md-4 text-center" data-aos="fade-up" data-aos-duration="1000">
+                    <div className="col-md-4 text-center" data-aos="fade-left" data-aos-duration="1000">
                         <a href="https://www.google.com/maps/place/Shreeram+Hospital+%26+Eye+Care+Center/@23.2099807,82.2021081,17.78z/data=!4m5!3m4!1s0x39862abd2235bb27:0x8b01c03e322d1696!8m2!3d23.21013!4d82.20238" target="_blank">
                             <img className="contact-map" src={map} alt="google map"/>
                         </a>
